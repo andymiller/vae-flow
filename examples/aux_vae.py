8e7f9eddd09713c2a4fa077949403240a7e02c7d
Andrew Miller <andrew.colin.miller@gmail.com>
2016-07-12 16:20:25 -0400

    fixed reconstruction plotting

--- examples/aux_vae.py
+++ examples/aux_vae.py
@@ -86,19 +86,24 @@ def callback(itr):
         import numpy as np
         z = np.array(np.random.randn(num_samps, zdim), dtype=np.float32)
         return decode(z).eval(session=sess)
-    viz.plot_samples(itr, samplefun, savedir='vae_mnist_samples')
+    viz.plot_samples(itr, samplefun, savedir='avae_mnist_samples')
 
     def sample_z(mu, log_sigmasq, M=5):
         eps = tf.random_normal((M, zdim), dtype=tf.float32)
         return mu + tf.exp(0.5 * log_sigmasq) * eps
 
     def recons(num_samps):
-        # random subset
+        # choose one datapoint, encode it
         subset = X[np.random.choice(X.shape[0], 1)]
-        mu, log_sigmasq = encode(subset)
-        imgs = decode(sample_z(mu, log_sigmasq, M=24)).eval(session=sess)
+        # compute encoder - first aux, then encoder conditioned on aux
+        amu, alog_sigmasq = aux_encode(subset)
+        asamps            = sample_z(amu, alog_sigmasq, M=24)
+        Xa                = tf.concat(1, [tf.tile(subset, [24, 1]),  asamps])
+        zmu, zlog_sigmasq = encode(Xa)
+        imgs = decode(sample_z(zmu, zlog_sigmasq, M=24)).eval(session=sess)
         return np.row_stack([subset, imgs])
-    viz.plot_samples(itr, recons, savedir='vae_mnist_samples', stub='recon')
+
+    viz.plot_samples(itr, recons, savedir='avae_mnist_samples', stub='recon')
     test_lb = test_lb_fun.eval(session=sess) * Ntest
     print "test data VLB: ", np.mean(test_lb)
 
